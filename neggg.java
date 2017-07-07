da9ed5b5a0263ed3b8e548c3abb9e5cdb1e03778
Priyadharsini123 <priyadharsinisigamani66@gmail.com>
2017-07-07 15:16:15 +0530

    Create neggg.java

---
+++ neggg.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class neg
+{
+public static void main(String []args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt()
+if(n>0)
+{
+System.out.print(n);
+}
+else
+{
+Sytem.out.print("0");
+}
+}
+}
